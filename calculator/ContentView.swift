3f88b1005a99b02bde03f71ff679a25d3bcc9ee8
sfukagawa <johannes.violoncello@gmail.com>
2020-09-03 00:24:51 +0900

    Initial Commit

---
+++ calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by 深川晴登 on 2020/09/03.
+//  Copyright © 2020 深川晴登. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
